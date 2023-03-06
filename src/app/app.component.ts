6a640050a102ab70cac25faae97e7102a4cb3438
Diego <diego.gonzalez.ortiz@gmail.com>
2023-03-07 02:41:27 +1000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,65 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'DiegoAngular';
+  style='angular';
+  technologyStack=[
+    {
+      name: 'Angular',
+      img: '/assets/angular-icon.svg',
+      detail: $localize`I've been an Angular developer for about 5 years`
+    },
+    {
+      name: 'Spring',
+      img: '/assets/spring-icon.svg',
+      detail: ''
+    },
+    {
+      name: 'Postgresql',
+      img: '/assets/Postgresql_elephant.svg',
+      detail: ''
+    },
+    {
+      name: 'Git',
+      img: '/assets/Git_icon.svg',
+      detail: ''
+    },
+    {
+      name: 'Ionic',
+      img: '/assets/ionic-icon-svgrepo-com.svg',
+      detail: ''
+    },
+    {
+      name: 'Java',
+      img: '/assets/pngfind.com-mysql-logo-transparent-png-744402.png',
+      detail: ''
+    },
+    {
+      name: 'Jira',
+      img: '/assets/jira-3.svg',
+      detail: ''
+    },
+    {
+      name: 'Bootstrap',
+      img: '/assets/Bootstrap_logo.svg',
+      detail: ''
+    },
+    {
+      name: 'Node',
+      img: '/assets/Node.js_logo.svg',
+      detail: ''
+    },
+    {
+      name: 'React',
+      img: '/assets/React-icon.svg',
+      detail: ''
+    },
+
+  ];
+  constructor(){
+
+  }
+
+  setStyle(style:string){
+    this.style=style;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+    MatGridListModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
